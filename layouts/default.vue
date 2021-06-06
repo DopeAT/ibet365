9d4c100807c7807d1b5964bcae394906d36f2bf4
Alex Tser <alextser@yahoo.co.uk>
2021-06-06 18:54:46 +0100

    * Nuxt App with Buefy

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Navbar />
+
+    <div class="container column is-10">
+      <nuxt />
+    </div>
+
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Navbar from '~/components/global/Navbar'
+import Footer from '~/components/global/Footer'
+
+export default {
+  components: { Navbar, Footer },
+  data () {
+    return {}
+  }
+}
+</script>
---
+++ nuxt.config.js
@@ -0,0 +1,84 @@
+const axios = require('axios')
+
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'ibet365.co.uk',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/dotenv',
+    'nuxt-compress'
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/buefy
+    'nuxt-buefy',
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+    '@nuxtjs/style-resources',
+    [
+      'nuxt-compress',
+      {
+        gzip: {
+          threshold: 8192
+        },
+        brotli: {
+          threshold: 8192
+        }
+      }
+    ]
+  ],
+
+  // add this new object if it doesn't exist already
+  styleResources: {
+    scss: ['./assets/scss/*.scss']
+  },
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    baseURL: 'http://localhost:3000'
+  },
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en'
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    babel: {
+      compact: true
+    }
+  }
+}
